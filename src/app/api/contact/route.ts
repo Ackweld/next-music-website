3fe82a424f02a207a35c42b662f7050dbe7db2dd
Ackweld <Axel-bj@hotmail.com>
2024-01-17 15:48:50 +0100

    Fixed wrong argument in Post method

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 
 // Handles POST requests to /api
 
-export async function POST(request: NextResponse) {
+export async function POST(request: NextRequest) {
   const username = process.env.NEXT_PUBLIC_BURNER_USERNAME;
   const password = process.env.NEXT_PUBLIC_BURNER_PASSWORD;
   const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
