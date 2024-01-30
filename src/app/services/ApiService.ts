e5163e738fdbc8d5b4667059aecc15ab66d19c2a
Ackweld <Axel-bj@hotmail.com>
2024-01-30 15:58:14 +0100

    Changed env variable again

--- src/app/services/ApiService.ts
+++ src/app/services/ApiService.ts
@@ -1,7 +1,7 @@
 export const ApiService = {
   getData: async (endpoint: string) => {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_DOMAIN}/api/contentful/${endpoint}`,
+      `${process.env.NEXT_PUBLIC_SITE_URL}/api/contentful/${endpoint}`,
       { headers: { Accept: "application/json", method: "GET" } }
     );
 
